d39a9bbe02c518588df0547f7eedd0ee55849b0c
prafful <prraful@gmail.com>
2018-03-28 13:02:41 +0530

    added yahoofinanceapi integration - not working for now

---
+++ 016_microservice_stock_yahoofinance/src/main/java/com/microservice/stockservice/entry/SpringBootMainEntryYahooFinance.java
@@ -0,0 +1,14 @@
+package com.microservice.stockservice.entry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMainEntryYahooFinance {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SpringApplication.run(SpringBootMainEntryYahooFinance.class, args);
+	}
+
+}
---
+++ 016_microservice_stock_yahoofinance/src/main/java/com/microservice/stockservice/getyahooquotes/GetYahooStockQuotes.java
@@ -0,0 +1,52 @@
+package com.microservice.stockservice.getyahooquotes;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import yahoofinance.Stock;
+import yahoofinance.YahooFinance;
+
+@RestController
+@RequestMapping("/rest/getyahoo")
+public class GetYahooStockQuotes {
+
+	@Autowired
+	private RestTemplate restTemplate;	
+	
+	@RequestMapping("/{username}")
+	public List<Stock> getStockQuote(@PathVariable("username") String username) {
+		
+		ResponseEntity<List<String>> yahooQuoteResponse = restTemplate
+				.exchange("http://localhost:8181/stocks/get/quotes/" + username, HttpMethod.GET, null, 
+						new ParameterizedTypeReference<List<String>>() {
+		});
+		
+		List<String> yahooQuotes = yahooQuoteResponse.getBody();
+		return yahooQuotes.stream().map(this::getQuotePrice).collect(Collectors.toList());
+		 
+		
+				
+				
+	}
+	
+	private Stock getQuotePrice(String quote) {
+		try {
+			return YahooFinance.get(quote);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return new Stock(quote);
+	}
+	
+}
