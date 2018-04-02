65977f21c0a108cd7b4050f0401be21f36222f94
prafful <prraful@gmail.com>
2018-04-02 11:21:17 +0530

    microservices are working with eureka server, cloud, zuul and frontend in react

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-
-import Website from './Website/Website';
-
-ReactDOM.render(
-    <Website></Website>, document.getElementById('spaContent')
-);
